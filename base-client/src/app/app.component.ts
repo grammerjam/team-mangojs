9e561d11dc3dd476738fcf78801b7037c2c41f61
ssmodish <stevesmodish@gmail.com>
2024-08-01 16:58:53 -0400

    rearranged pages and components for better organization

--- base-client/src/app/app.component.ts
+++ base-client/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { HomepageComponent } from "./homepage/homepage.component";
-import { MoviesComponent } from "./movies/movies.component";
-import { SeriesComponent } from "./series/series.component";
-import { BookmarksComponent } from "./bookmarks/bookmarks.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { HomepageComponent } from "./pages/homepage/homepage.component";
+import { MoviesComponent } from "./pages/movies/movies.component";
+import { SeriesComponent } from "./pages/series/series.component";
+import { BookmarksComponent } from "./pages/bookmarks/bookmarks.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
-	selector: "app-root",
-	standalone: true,
-	imports: [RouterOutlet, HomepageComponent, MoviesComponent, SeriesComponent, BookmarksComponent, NavbarComponent],
-	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.scss",
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterOutlet, HomepageComponent, MoviesComponent, SeriesComponent, BookmarksComponent, NavbarComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-	title = "Mango Entertainment";
+  title = "Mango Entertainment";
 }
--- base-client/src/app/app.routes.ts
+++ base-client/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
-import { MoviesComponent } from './movies/movies.component';
-import { SeriesComponent } from './series/series.component';
-import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { SeriesComponent } from './pages/series/series.component';
+import { BookmarksComponent } from './pages/bookmarks/bookmarks.component';
 
 export const routes: Routes = [
-  {path: '', component: HomepageComponent},
-  {path: 'movies', component: MoviesComponent},
-  {path: 'series', component: SeriesComponent},
-  {path: 'bookmarks', component: BookmarksComponent},
+  { path: '', component: HomepageComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'series', component: SeriesComponent },
+  { path: 'bookmarks', component: BookmarksComponent },
 ];
