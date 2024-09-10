18c0002964dc642197a38d46d18d94642975bd45
alexvcs <alexcurtisslep@gmail.com>
2024-09-10 18:43:44 +0000

    regular card styles in the works
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- base-client/src/app/components/regular-card/regular-card.component.ts
+++ base-client/src/app/components/regular-card/regular-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { SelectionData } from '../../interfaces/selection-data';
+import { BookmarkComponent } from "../bookmark/bookmark.component";
 
 @Component({
   selector: 'app-regular-card',
   standalone: true,
-  imports: [],
+  imports: [BookmarkComponent],
   templateUrl: './regular-card.component.html',
   styleUrl: './regular-card.component.scss'
 })
--- base-client/src/app/components/regular-section/regular-section.component.ts
+++ base-client/src/app/components/regular-section/regular-section.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SelectionData } from '../../interfaces/selection-data';
+import { RegularCardComponent } from "../regular-card/regular-card.component";
 
 @Component({
   selector: 'app-regular-section',
   standalone: true,
-  imports: [],
+  imports: [RegularCardComponent],
   templateUrl: './regular-section.component.html',
   styleUrl: './regular-section.component.scss'
 })
 export class RegularSectionComponent {
-
+  @Input() sectionTitle: string = ''
+  @Input() sectionData: SelectionData[] = []
 }
