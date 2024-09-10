97cc83412f6b51de896d57caf71e3f166e967441
alexvcs <alexcurtisslep@gmail.com>
2024-09-10 18:05:12 +0000

    create bookmark component, finish styling trending-card and section
    Co-authored-by: Steve Smodish <stevesmodish@gmail.com>

--- base-client/src/app/components/trending-card/trending-card.component.ts
+++ base-client/src/app/components/trending-card/trending-card.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { SelectionData } from '../../interfaces/selection-data';
+import { BookmarkComponent } from "../bookmark/bookmark.component";
 
 @Component({
   selector: 'app-trending-card',
   standalone: true,
-  imports: [],
+  imports: [BookmarkComponent],
   templateUrl: './trending-card.component.html',
   styleUrl: './trending-card.component.scss'
 })
 export class TrendingCardComponent {
   @Input() selectionData: SelectionData = { id: '', title: '', thumbnail: { trending: { small: '', large: '' }, regular: { small: '', medium: '', large: '' } }, category: '', rating: '', isBookmarked: false, isTrending: false, year: 0 }
-  
 }
